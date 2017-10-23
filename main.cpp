0897243156a064739e53676f4b8d282e93c70ae9
fiiniefi <marcin.drewniak1@vp.pl>
2017-10-23 02:00:38 +0200

    Main fixed

--- main.cpp
+++ main.cpp
@@ -1,10 +1,12 @@
-#include <pthread.h>
+﻿#include <pthread.h>
 #include <iostream>
+#include <unistd.h>
+#include <cstdio>
 
 void *hello(void *thread_number)
 {
-    int numb = int(thread_number);
-    std::cout << "Thread number: " << numb << std::endl;
+    int *numb = static_cast<int*>(thread_number);
+    std::cout << "Thread number: " << *numb << std::endl;
     std::cout.flush();
     pthread_exit(NULL);
 }
@@ -12,11 +14,15 @@ void *hello(void *thread_number)
 
 int main()
 {
-    const int length = 100;
+    const int length = 101;
     pthread_t threads[length];
+    int thread_vals[length];
     for (int i = 0; i < length; i++)
-        if (pthread_create(&threads[i], NULL, &hello, (void *)i))
+    {
+        thread_vals[i] = i;
+        if (pthread_create(&threads[i], NULL, &hello, &thread_vals[i]))
             std::cout << "Error: Thread couldn't be created." << std::endl;
+    }
     std::cout << "Main thread." << std::endl;
     std::cout.flush();
     pthread_exit(NULL);
